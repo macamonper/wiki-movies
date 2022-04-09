e63bb03d1a8a2b097837b5a5547f2435d536dcf0
macarena <macamonper@gmail.com>
2022-04-09 04:11:36 -0300

    add info to series

--- src/components/info/DetailedInfo.js
+++ src/components/info/DetailedInfo.js
@@ -11,12 +11,13 @@ import ToolBar from "./Toolbar";
 import { Box } from "@mui/system";
 import Stars from "./Stars"
 import Genres from "./Genres"
+import { currencyFormat } from "../../auxiliars/functions";
 
 const DetailedInfo = () => {
 
   const params = useParams()
   const info = useFetchInfo()
-
+console.log(info.homepage)
 
   return (
     <Card>
@@ -50,37 +51,47 @@ const DetailedInfo = () => {
             <Typography variant="h4" color={"#FFFFFF"} fontSize={30} py={2}>
               {info.title}
             </Typography>
-
             <Stars value={`${info.vote_average}`}></Stars>
-
             {info.overview != null && (
               <Typography variant="h5" color={"#F5F3F4"} fontSize={20} py={1}>
                 {info.overview}
               </Typography>
             )}
-
             {info.runtime > 1 && (
               <Typography variant="h5" color={"#F5F3F4"} fontSize={20} py={1}>
                 Duration: {info.runtime} minutes
               </Typography>
             )}
-
             {info.release_date != null && (
               <Typography variant="h5" color={"#F5F3F4"} fontSize={20} py={1}>
                 Release date: {info.release_date}
               </Typography>
             )}
-
-            {info.budget > 1 && (
+            {info.first_air_date && (
               <Typography variant="h5" color={"#F5F3F4"} fontSize={20} py={1}>
-                Budget: ${info.budget}
+                Release date: {info.first_air_date}
               </Typography>
             )}
-
+            {info.budget > 1 && (
+              <Typography variant="h5" color={"#F5F3F4"} fontSize={20} py={1}>
+                Budget: {currencyFormat(info.budget)}
+              </Typography>
+            )}
             {info.revenue > 1 && (
               <Typography variant="h5" color={"#F5F3F4"} fontSize={20} py={1}>
-                Revenue: ${info.revenue}
+                Revenue: {currencyFormat(info.revenue)}
               </Typography>
+            )}
+            {info.number_of_episodes && (
+              <Typography variant="h5" color={"#F5F3F4"} fontSize={20} py={1}>
+                Episodes: {info.number_of_episodes}
+              </Typography>
+            )}
+            {info.number_of_season && (
+              <Typography variant="h5" color={"#F5F3F4"} fontSize={20} py={1}>
+                Seasons: {info.number_of_season}
+              </Typography>
+            )}
             )}
           </Box>
         </Container>
